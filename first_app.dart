6c3599dc2795409c5634791bde4fb61fa2f392e3
WatcharapolxFa <63010870@kmitl.ac.th>
2023-09-28 22:00:01 +0700

    feat: start couse

---
+++ first_app.dart
@@ -0,0 +1,7 @@
+void main (List<String> args) {
+  print('Hello World');
+  print('Hi');
+  print('1+3');
+  print(1+3);
+  print('Hello'+'world');
+}
---
+++ variables_dataTypes.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  int number1 = 10;
+  print(number1);
+  double number2 = 10.5;
+  print(number2);
+}
